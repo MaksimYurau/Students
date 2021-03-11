3aadda33763d76abf19034ffce6bbbfd95c2a2be
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-03-11 21:46:48 +0300

    Added src files.

---
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.students;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.activity_main_recycler_view);
+        FloatingActionButton floatingActionButton = findViewById(R.id.activity_main__fb_add);
+//        floatingActionButton.setColorFilter(Color.argb(255, 255, 255, 255));
+
+    }
+}
