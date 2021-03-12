d5f177f380556a399a15aa1f80854903ac758881
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-03-12 17:52:37 +0300

    Project update. Added student_item.xml. Updated MainActivity.java. Updated strings.xml. Updated build.gradle.

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.students;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+    private TextView textView;
+    private EditText firstName;
+    private EditText lastName;
+    private CheckBox isMale;
+    private Button saveButton;
+    private Button deleteButton;
+    private CircleImageView circleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        RecyclerView recyclerView = findViewById(R.id.activity_main_recycler_view);
-        FloatingActionButton floatingActionButton = findViewById(R.id.activity_main__fb_add);
+        setContentView(R.layout.student_item);
     }
 }
