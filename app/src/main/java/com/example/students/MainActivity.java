8e23b2282c32034f7c4dabf993a2e03bc4006b2d
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-03-12 18:21:30 +0300

    Project update. Updated MainActivity.java.

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,14 +15,13 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private FloatingActionButton floatingActionButton;
-    private TextView textView;
+    private FloatingActionButton addButton;
     private EditText firstName;
     private EditText lastName;
-    private CheckBox isMale;
+    private CheckBox gender;
     private Button saveButton;
     private Button deleteButton;
-    private CircleImageView circleImageView;
+    private CircleImageView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
